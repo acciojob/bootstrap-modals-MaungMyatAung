649bf3263b0164690b6fa15798937bca7375476f
MaungMyatAung <85542468+MaungMyatAung@users.noreply.github.com>
2025-04-15 10:36:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+ document.addEventListener('DOMContentLoaded', () => {
+            const openModalButton = document.getElementById('open-modal');
+            const newtonModal = new bootstrap.Modal(document.getElementById('newton-modal'));
+
+            openModalButton.addEventListener('click', () => {
+                newtonModal.show();
+            });
+        });
